ddfa0a7782bfed3cd12ae42d5ee3b0ee82ca7020
Luckycarrot42 <eicarriemontecinos@gmail.com>
2020-02-19 22:01:38 -0300

    Add files via upload
    19-02

---
+++ portafolio/assets/js/script.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+
+
+    $(function(){
+        $("a").click(function(event) {
+            if (this.hash !== ""){
+                event.preventDefault();
+                var gato = this.hash;
+                console.log(gato);
+                $('html, body').animate({
+                    scrollTop: $(gato).offset().top
+                }, 800, function(){
+                    window.location.hash = gato;
+                });
+            }
+        });
+    });
+
+
+});
+    
